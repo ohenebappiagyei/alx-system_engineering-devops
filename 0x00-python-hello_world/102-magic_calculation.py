5f7ef75416667404c5b9f20530c926c84eaf4ddf
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-02 13:49:04 -0700

    worked?

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    result = 98 # LOAD_CONST 1 (98)
-
-    # LOAD_FAST 0 (a)
-    # LOAD_FAST 1 (b)
-    # BINARY_POWER
-    # BINARY_ADD
-    result **= a
-    result += b
-
-    return (result)
+    return (98 + a ** b)
