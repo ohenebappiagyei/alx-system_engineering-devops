7fd26a860c914b6b79ac46440ab43e6c5b795e5f
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-03 10:21:45 -0700

    last last

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10
 if number < 0:
-    last_digit = -last_digit
+    last_digit = -(-number % 10)
 else:
-    last_digit = last_digit
+    last_digit = number % 10
 
 if last_digit == 0:
     result = "and is 0"
