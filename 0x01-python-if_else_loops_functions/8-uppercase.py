c14eba9098deccc92d6abb1a0d0fb69840b66013
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-03 12:03:39 -0700

    trying

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    for char in s:
+        uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print("{}".format(uppercase_char), end='')
+    print()
