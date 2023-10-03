952cfd2c249414164197b7bc2919e3c38faf350f
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-03 11:10:50 -0700

    corrected output

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(100):
     if i < 10:
-        print("0{:d}".format(i), end="," if i < 99 else "\n")
+        print("0{:d}".format(i), end=", " if i < 99 else "\n")
     else:
-        print("{:d}".format(i), end="," if i < 99 else "\n")
+        print("{:d}".format(i), end=", " if i < 99 else "\n")
