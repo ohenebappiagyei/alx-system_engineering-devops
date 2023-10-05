3fe8733af74b227dcbd8f78a7ef147e490555418
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-05 14:32:45 -0700

    solved

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     total_sum = 0
 
     if len(argv) > 1:  # Check if there are arguments
-        for i in range(1, len(argv)):  # Start from index 1 to skip the script name
+        for i in range(1, len(argv)):
+            # Start from index 1 to skip the script name
             total_sum += int(argv[i])
 
         print("{}".format(total_sum))
     else:
         print("0")
-
