f8b95dce24c15d76514b96e9aff4607a1bc89789
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-05 15:54:15 -0700

    corrected

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+from variable_load_5 import a
 
-
-import importlib.util
-
-
-def import_variable_from_file(filename):
-    spec = importlib.util.spec_from_file_location("variable_module", filename)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return (module.a)
-
+def find_var():
+    print(a)
 
 if __name__ == "__main__":
-    value_of_a = import_variable_from_file("variable_load_5.py")
-    print(value_of_a)
+    find_var()
