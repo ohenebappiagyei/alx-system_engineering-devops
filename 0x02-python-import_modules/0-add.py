2c73a7ae54a77e50acc6cf3b97ffc2f84da63732
Dr. Appiagyei <134808240+ohenebappiagyei@users.noreply.github.com>
2023-10-05 14:53:46 +0000

    Update 0-add.py 2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
+	# Import module
+	from add_0 import add
+
 	# Assigning variables
 	a = 1
 	b = 2
-
-	# Import module
-	from add_0 import add
 
 	print("{:d} + {:d} = {:d}".format(a,b,(add(a, b))))
