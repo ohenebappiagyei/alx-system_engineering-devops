b467d648b05087589418af3acd689398f5df234e
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-23 15:08:15 -0700

    done

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,7 +3,6 @@
 
 def magic_calculation(a, b):
     result = 0
-    
     for i in range(1, 3):
         try:
             if i > a:
@@ -12,5 +11,5 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-    
+
     return result
