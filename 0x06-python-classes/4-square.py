8d68fe69662bf68897e50215e9ff3d60a889f6ca
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-10-24 12:53:15 -0700

    trying

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size=0):
+        """Initialize a new square with an optional size."""
+        self.size = size
+
+        @property
+        def size(self):
+            """Retrieve the size of the square."""
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            """Set the size of the square with type and value validation."""
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+        def area(self):
+            """Calculate and return the area of the square."""
+            return self.__size ** 2
