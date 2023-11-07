dc9d5e15d0c2c023904dc42cf923d394b366a2a9
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-07 21:54:48 +0000

    one day

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-"""
-    script that adds all arguments to a Python list,
-    and then save them to a file:
-"""
-
-
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-arg = sys.argv[1:]
-file_name = "add_item.json"
 
-try:
-    python_object = load_from_json_file(file_name)
-except FileNotFoundError:
-    save_to_json_file([], file_name)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    for arg in sys.argv[1:]:
+        items.append(arg)
 
-python_object = load_from_json_file(file_name)
-if type(python_object) is list:
-    for item in arg:
-        python_object.append(item)
-
-save_to_json_file(python_object, file_name)
+    save_to_json_file(items, "add_item.json")
