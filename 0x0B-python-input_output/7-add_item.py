ae9c86d9e4b9079996dfe9f2ff461464a8f5da9b
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-07 21:48:06 +0000

    try once again

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,6 @@
 """
     script that adds all arguments to a Python list,
     and then save them to a file:
-
-    You must use your function save_to_json_file from
-    7-save_to_json_file.py
-    You must use your function load_from_json_file
-    from 8-load_from_json_file.py
-    The list must be saved as a JSON representation
-    in a file named add_item.json
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions / exceptions.
 """
 
 
