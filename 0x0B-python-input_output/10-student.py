b618f09d629e32312a79b6090aa59ae575c055b1
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-11-08 00:18:07 +0000

    till its done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A class 'Student' that defines a student by
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list, optional):
+            A list of strings representing the attributes to retrieve.
+                If None, all attributes will be retrieved.
+
+        Returns:
+            dict: A dictionary
+            containing the specified attributes of the student.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {
+            attr: getattr(self, attr)
+            for attr in attrs
+            if hasattr(self, attr)
+        }
