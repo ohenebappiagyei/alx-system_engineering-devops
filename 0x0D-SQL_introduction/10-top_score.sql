2dfe84831922524ae60bfdb0bbf6fbcb92674000
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-12-12 13:30:16 -0800

    list by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records of the table second_table in the specified database
+-- displaying both the score and the name, ordered by score in descending order
+
+SELECT score, name FROM second_table ORDER BY score DESC;
