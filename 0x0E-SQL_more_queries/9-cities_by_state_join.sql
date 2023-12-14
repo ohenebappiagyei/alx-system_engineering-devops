cecc9653694ffcfd392beb37dcdde726a67f8d04
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2023-12-13 16:28:34 -0800

    Cities by states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- List all cities with their corresponding state names
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
