44fd64daac95bd14456abc44bbe4f804b3c835e9
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-08 14:32:59 +0000

    square done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+const size = parseInt(arg);
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
