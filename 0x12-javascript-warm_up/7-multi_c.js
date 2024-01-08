bdf75b95ed00183e4468ae4ace178e4e7c4d5e12
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-08 14:21:00 +0000

    task 7 updated

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 const x = parseInt(arg);
-if (!isNaN(x) && x > 0) {
+if (!isNaN(x)) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
