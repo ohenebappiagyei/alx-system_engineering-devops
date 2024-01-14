64b825e8605b22f9a4547829f134eb8a6c1db33a
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-01-14 01:05:47 -0800

    Try 5 again

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all cities of that state,
-using the database hbtn_0e_4_usa.
+Script that lists all states from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
 import sys
 
-
-def filter_cities_by_state(username, password, database, state_name):
+def fetch_states(username, password, database, state_name):
     """
-    Lists all cities for the given state from the specified database, sorted by id.
+    Lists all states from the specified database that match the given state_name.
     """
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=password, db=database)
 
     # Create a cursor object using cursor() method
-    c = db.cursor()
+    cur = db.cursor()
 
-    # Execute SQL query to retrieve cities with the specified state
-    c.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') FROM cities "
-              "JOIN states ON cities.state_id = states.id "
-              "WHERE states.name = %s", (state_name,))
+    # Execute SQL query with state_name as a parameter
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (state_name,))
 
-    # Fetch the result and print it
-    result = c.fetchone()
-    if result[0]:
-        print(result[0])
+    # Fetch all rows and print the results
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
 
-    # Close the cursor and disconnect from server
-    c.close()
+    # Close the cursor and disconnect from the server
+    cur.close()
     db.close()
 
-
 if __name__ == "__main__":
-    # Check if correct number of arguments provided
+    # Check if the correct number of arguments is provided
     if len(sys.argv) == 5:
         username = sys.argv[1]
         password = sys.argv[2]
         database = sys.argv[3]
         state_name = sys.argv[4]
-        filter_cities_by_state(username, password, database, state_name)
+        fetch_states(username, password, database, state_name)
     else:
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+
